e2c04a68ce17da71049ee4cdf75cbfa1abdfb778
dimaNuts <nuts272@mail.ru>
2023-11-26 13:46:38 +0300

    Create form for create post
    *add input Создание поста
    *add input Описание поста
    *add button Создать пост

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ function App() {
 
   return (
     <div className="App">
+      <form>
+        <input type='text' placeholder='Название поста'/>
+        <input type='text' placeholder='Описание поста'/>
+        <button>Создать пост</button>
+      </form>
       <PostList posts={posts} title='Список постов'/>
     </div>
   );
